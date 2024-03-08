fe38135257f3e837eba292f5fc667d46c63187b2
supSugam <sugamsubedi234@gmail.com>
2024-03-08 22:09:25 +0545

    Refactor app.controller.ts and create-playlist.dto.ts

--- src/playlists/dto/create-playlist.dto.ts
+++ src/playlists/dto/create-playlist.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePlaylistDto {}
+export class CreatePlaylistDto {
+  //     title         String
+  //   description   String?
+  //   cover         String?
+  // tracks        Track[] // Tracks in this playlist
+  //   publicStatus  ReviewStatus?   @default(NOT_REQUESTED)
+  //   tags          Tag[]
+  //  collaborators User[]          @relation("Collaborators")
+}
